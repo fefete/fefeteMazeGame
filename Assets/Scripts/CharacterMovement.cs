1ade6db2228d7d2f26ad8921b23dd99088ea2de0
Fernando Ferrando Terradez <metrack92@gmail.com>
2019-08-06 01:20:16 +0200

    Removed unity chan and first working movement iteration

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,43 +4,145 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    CharacterController characterController;
+    public enum CharacterMovementState : uint
+    {
+        Default = 0x0001,
+        Running = 0x0010,
+        Walking = 0x0100,
+    }
 
-    public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-    public float rotSpeed = 90;
+    CharacterController oCharacterController;
 
-    private Vector3 moveDirection = Vector3.zero;
+    public float fSpeed = 6.0f;
+    public float fRunningSpeed = 3.0f;
+    public float fWalkingSpeed = .5f;
+    public float fStandardSpeed = 1.0f;
+    public float fJumpSpeed = 8.0f;
+    public float fGravity = 20.0f;
+    public float fRotSpeed = 90;
+    public float fCrouchDeltaHeight = 0.15f;
+    public const float fMaxStamina = 10.0f;
+    public float fStamina = fMaxStamina;
+    public float fStaminaRecoveryRate = 5f;
+    public float fStaminaSpendingRate = 1f;
+    public bool bTired = false;
+    delegate void OnMovementStateChangeDelegate(CharacterMovementState _eOldState, CharacterMovementState _eNewState);
+    event System.Action<CharacterMovementState, CharacterMovementState> OnMovementStateChange;
+
+    CharacterMovementState eState = CharacterMovementState.Default;
+
+
+    private Vector3 vMoveDirection = Vector3.zero;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        oCharacterController = GetComponent<CharacterController>();
+        AddToMovementStateChangeCaller(OnMovementStateChanged);
     }
 
     void Update()
     {
-        if (characterController.isGrounded)
+        if (Input.GetKey(KeyCode.LeftShift) && !bTired)
+        {
+            if (eState != CharacterMovementState.Running)
+            {
+                fSpeed = fRunningSpeed;
+                setMovementState(CharacterMovementState.Running);
+            }
+            fStamina -= fStaminaSpendingRate * Time.deltaTime;
+            if (fStamina <= 0)
+            {
+                bTired = true;
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            if (eState != CharacterMovementState.Walking)
+            {
+                fSpeed = fWalkingSpeed;
+                setMovementState(CharacterMovementState.Walking);
+            }
+        }
+        else if (eState != CharacterMovementState.Default || (fStamina <= 0 && eState == CharacterMovementState.Running))
+        {
+            fSpeed = fStandardSpeed;
+            setMovementState(CharacterMovementState.Default);
+        }
+
+        if (eState != CharacterMovementState.Running && fStamina <= fMaxStamina)
+        {
+            fStamina += fStaminaRecoveryRate * Time.deltaTime;
+            if (fStamina > fMaxStamina)
+            {
+                fStamina = fMaxStamina;
+                bTired = false;
+            }
+        }
+
+        if (oCharacterController.isGrounded)
         {
             // We are grounded, so recalculate
             // move direction directly from axes
 
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
+            vMoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            vMoveDirection = transform.TransformDirection(vMoveDirection);
+            vMoveDirection *= fSpeed;
 
             if (Input.GetButton("Jump"))
             {
-                moveDirection.y = jumpSpeed;
+                vMoveDirection.y = fJumpSpeed;
             }
         }
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
         // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
         // as an acceleration (ms^-2)
-        moveDirection.y -= gravity * Time.deltaTime;
+        vMoveDirection.y -= fGravity * Time.deltaTime;
 
         // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
+        oCharacterController.Move(vMoveDirection * Time.deltaTime);
+    }
+
+
+    CharacterMovementState getMovementState()
+    {
+        return eState;
+    }
+    void setMovementState(CharacterMovementState _eNewState)
+    {
+        if (eState == _eNewState) return;
+        OnMovementStateChange(eState, _eNewState);
+        eState = _eNewState;
+    }
+
+    void OnMovementStateChanged(CharacterMovementState _eOldState, CharacterMovementState _eNewState)
+    {
+        switch (_eNewState)
+        {
+            case CharacterMovementState.Default:
+                if (_eOldState == CharacterMovementState.Walking)
+                {
+                    GetComponent<CharacterController>().radius += fCrouchDeltaHeight;
+                    //GetComponent<BoxCollider>().center += new Vector3(0, fCrouchDeltaHeight / 2, 0);
+                }
+                break;
+            case CharacterMovementState.Running:
+                if (_eOldState == CharacterMovementState.Walking)
+                {
+                    GetComponent<CharacterController>().radius += fCrouchDeltaHeight;
+                    //GetComponent<BoxCollider>().center += new Vector3(0, fCrouchDeltaHeight / 2, 0);
+                }
+                break;
+            case CharacterMovementState.Walking:
+                GetComponent<CharacterController>().radius -= fCrouchDeltaHeight;
+                //GetComponent<BoxCollider>().center -= new Vector3(0, fCrouchDeltaHeight / 2, 0);
+                break;
+        }
+    }
+
+    public void AddToMovementStateChangeCaller(System.Action<CharacterMovementState, CharacterMovementState> _oFunc)
+    {
+        OnMovementStateChange += _oFunc;
     }
 }
--- Assets/Scripts/CharacterRotation.cs
+++ Assets/Scripts/CharacterRotation.cs
@@ -4,47 +4,67 @@ using UnityEngine;
 
 public class CharacterRotation : MonoBehaviour
 {
-    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-    public RotationAxes axes = RotationAxes.MouseXAndY;
-    public float sensitivityX = 15F;
-    public float sensitivityY = 15F;
+    enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    RotationAxes axes = RotationAxes.MouseXAndY;
+    public float fSensitivityX = 15F;
+    public float fSensitivityY = 15F;
+    public float fSensitivityZ = 2F;
 
-    public float minimumX = -360F;
-    public float maximumX = 360F;
+    public float fMinimumX = -360F;
+    public float fMaximumX = 360F;
 
-    public float minimumY = -60F;
-    public float maximumY = 60F;
+    public float fMinimumY = -60F;
+    public float fMaximumY = 60F;
 
-    float rotationY = 0F;
+    public float fMinimumZ = -45F;
+    public float fMaximumZ = 45F;
 
-    void Update ()
+    float fRotationY = 0F;
+    float fRotationZ = 0F;
+    float frotationX = 0F;
+
+    public float fReturnRate = 1.0f;
+    public bool bRestoreRotation = false;
+    void Update()
     {
-        if (axes == RotationAxes.MouseXAndY)
+        if ((Input.GetAxis("Incline") != 0))
         {
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-
-            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-        }
-        else if (axes == RotationAxes.MouseX)
-        {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+            fRotationZ += (Input.GetAxis("Incline")) * fSensitivityZ;
+            fRotationZ = Mathf.Clamp(fRotationZ, fMinimumZ, fMaximumZ);
+            bRestoreRotation = false;
         }
         else
         {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-
-            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+            bRestoreRotation = true;
         }
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            frotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * fSensitivityX;
+            fRotationY += Input.GetAxis("Mouse Y") * fSensitivityY;
+            fRotationY = Mathf.Clamp(fRotationY, fMinimumY, fMaximumY);
+        }
+
+        if (bRestoreRotation)
+        {
+            fRotationZ = Mathf.Lerp(fRotationZ, 0, Time.deltaTime * fReturnRate);
+            if (fRotationZ == 0)
+            {
+                bRestoreRotation = false;
+            }
+            transform.localEulerAngles = new Vector3(-fRotationY, frotationX, fRotationZ);
+        }
+        else
+        {
+            transform.localEulerAngles = new Vector3(-fRotationY, frotationX, fRotationZ);
+        }
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
 }
